b8edf621a6f4856a8c78d54c8cccfb30a6a1c7f2
Bloody Pudding <bloodypudding@gmail.com>
2021-04-16 14:37:13 +0300

    links added

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,26 @@
 <template>
   <v-app>
     <v-navigation-drawer app v-model="drawer">
-      <v-drawer></v-drawer>
+      <v-drawer :links="links"></v-drawer>
     </v-navigation-drawer>
     <v-app-bar app>
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon
+        class="hidden-md-and-up"
+        @click.stop="drawer = !drawer"
+      ></v-app-bar-nav-icon>
       <v-toolbar-title>Vue-App-Ad</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn
+          text
+          v-for="(link, i) in links"
+          :key="i"
+          :to="link.url"
+        >
+          <v-icon left>{{ link.icon }}</v-icon>
+          {{ link.title }}
+        </v-btn>
+      </v-toolbar-items>
     </v-app-bar>
     <v-main>
       <v-container>
@@ -23,7 +38,14 @@ export default {
     vDrawer
   },
   data: () => ({
-    drawer: null
+    drawer: false,
+    links: [
+      { title: 'Login', icon: 'mdi-account-lock', url: '/login' },
+      { title: 'Registration', icon: 'mdi-face', url: '/registration' },
+      { title: 'Orders', icon: 'mdi-bookmark', url: '/orders' },
+      { title: 'New ad', icon: 'mdi-note-plus', url: '/new' },
+      { title: 'My ads', icon: 'mdi-clipboard-list', url: '/list' }
+    ]
   })
 }
 </script>
--- src/Drawer.vue
+++ src/Drawer.vue
@@ -1,5 +1,18 @@
 <template>
-  <v-list subheader two-line flat>
+  <v-list subheader two-line>
+    <v-subheader>Pages</v-subheader>
+    <v-list-item-group>
+      <v-list-item
+        v-for="(link, i) in links"
+        :key="i"
+        :to="link.url"
+      >
+        <v-icon v-text="link.icon" left></v-icon>
+        <v-list-item-content>
+          <v-list-item-title v-text="link.title"></v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list-item-group>
     <v-subheader>Theme settings</v-subheader>
     <v-list-item-group multiple>
       <v-list-item @change="$vuetify.theme.isDark = !$vuetify.theme.isDark">
@@ -21,6 +34,11 @@
 <script>
 export default {
   name: 'Drawer',
+  props: {
+    links: {
+      type: Array
+    }
+  },
   computed: {
     isDark () {
       return this.$vuetify.theme.isDark ? 'Dark' : 'Light'
