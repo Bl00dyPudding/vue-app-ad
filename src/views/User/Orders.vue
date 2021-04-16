2e11c4f5a20e6a3fb50681b9e37fd442c0059e0b
Bloody Pudding <bloodypudding@gmail.com>
2021-04-16 19:55:47 +0300

    orders page

--- src/views/User/Orders.vue
+++ src/views/User/Orders.vue
@@ -2,7 +2,31 @@
   <v-container>
     <v-layout row>
       <v-flex xs12>
-        <h1>Orders</h1>
+        <v-list>
+          <v-list-item
+            v-for="order in orders"
+            :key="order.id"
+          >
+            <v-list-item-action>
+              <v-checkbox
+                :input-value="order.done"
+                color="primary"
+                @click.stop="markAsDone(order)"
+                :disabled="order.done"
+              ></v-checkbox>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title>{{ order.name }}</v-list-item-title>
+              <v-list-item-subtitle>{{ order.phone }}</v-list-item-subtitle>
+            </v-list-item-content>
+            <v-list-item-icon>
+              <v-btn
+                :to="'/ad/' + order.adId"
+                class="primary"
+              >Open</v-btn>
+            </v-list-item-icon>
+          </v-list-item>
+        </v-list>
       </v-flex>
     </v-layout>
   </v-container>
@@ -12,8 +36,21 @@
 export default {
   name: 'Orders',
   data: () => ({
-    //
-  })
+    orders: [
+      {
+        id: 123,
+        adId: 321,
+        name: 'Vasya',
+        phone: '1234567890',
+        done: true
+      }
+    ]
+  }),
+  methods: {
+    markAsDone (order) {
+      order.done = true
+    }
+  }
 }
 </script>
 
