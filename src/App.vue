49b59d3c991fe8774649c05063e64352d12df50a
Bloody Pudding <bloodypudding@gmail.com>
2021-04-16 13:06:20 +0300

    drawer

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,29 @@
 <template>
   <v-app>
     <v-navigation-drawer app v-model="drawer">
-      <v-btn @click="$vuetify.theme.isDark = !$vuetify.theme.isDark">{{ isDark }}</v-btn>
+      <v-drawer></v-drawer>
     </v-navigation-drawer>
     <v-app-bar app>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Vue-App-Ad</v-toolbar-title>
     </v-app-bar>
     <v-main>
-      <v-container fluid>
+      <v-container>
         <router-view></router-view>
       </v-container>
     </v-main>
-    <v-footer app></v-footer>
   </v-app>
 </template>
 
 <script>
+import vDrawer from './Drawer'
 export default {
   name: 'App',
-
   components: {
-
+    vDrawer
   },
   data: () => ({
-    drawer: false
-  }),
-  computed: {
-    isDark () {
-      return this.$vuetify.theme.isDark ? 'Light' : 'Dark'
-    }
-  }
+    drawer: null
+  })
 }
 </script>
---
+++ src/Drawer.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-list subheader two-line flat>
+    <v-subheader>Theme settings</v-subheader>
+    <v-list-item-group multiple>
+      <v-list-item @change="$vuetify.theme.isDark = !$vuetify.theme.isDark">
+        <v-list-item-action>
+          <v-checkbox
+            :input-value="$vuetify.theme.isDark"
+            color="primary"
+          ></v-checkbox>
+        </v-list-item-action>
+        <v-list-item-content>
+          <v-list-item-title>Dark mode</v-list-item-title>
+          <v-list-item-subtitle>Current mode: {{ isDark }}</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list-item-group>
+  </v-list>
+</template>
+
+<script>
+export default {
+  name: 'Drawer',
+  computed: {
+    isDark () {
+      return this.$vuetify.theme.isDark ? 'Dark' : 'Light'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
